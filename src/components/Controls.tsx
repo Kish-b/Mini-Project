050069f05566797929675053570d25dc88d0d86e
kishore <121303390+Kish-b@users.noreply.github.com>
2025-03-20 23:07:30 +0530

    Add files via upload

---
+++ src/components/Controls.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Controls: React.FC = () => {
+  return <div className="controls">Draw an object or equation and click "Run".</div>;
+};
+
+export default Controls;
