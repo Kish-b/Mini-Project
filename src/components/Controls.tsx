4870b4ef857562ebd91c120636af331d50288e81
kishore <121303390+Kish-b@users.noreply.github.com>
2025-03-21 23:46:36 +0530

    Delete src directory

--- src/components/Controls.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Controls: React.FC = () => {
-  return <div className="controls">Draw an object or equation and click "Run".</div>;
-};
-
-export default Controls;
